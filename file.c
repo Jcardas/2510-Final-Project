15d51c350fba91349c7d0497f128d9e8b77bffd0
Felix Nguyen <pnguyen72@my.bcit.ca>
2025-04-03 15:09:12 -0700

    fix and format file.c

--- file.c
+++ file.c
@@ -14,13 +14,11 @@
 #include "patient.h"
 #include "patientsList.h"
 
-
 FILE *initializeFile()
 {
         FILE *dataFile = fopen(FILE_NAME, "r+");
-        if (dataFile == NULL) {
+        if (dataFile == NULL)
                 dataFile = fopen(FILE_NAME, "w+");
-        }
         return dataFile;
 }
 
@@ -29,55 +27,54 @@ void populatePatientNodesFromFile(PatientNodePtr patients, FILE *file)
         PatientNode *thisPatient = &patients; // Start from the first node
 
         char line[MAX_LINE_LENGTH];
-        while (fgets(line, sizeof(line), file) != NULL) { // Read through the file line by line.
+        while (fgets(line, sizeof(line), file) !=
+               NULL) { // Read through the file line by line.
                 {
-                      char* token = strtok(line, "|");
-                      int patientID = atoi(token); // The first data point is ID
+                        char *token = strtok(line, "|");
+                        int patientID =
+                                atoi(token); // The first data point is ID
 
-                      token = strtok(NULL, "|"); // Move to next token
-                      char* patientName = token;
+                        token = strtok(NULL, "|"); // Move to next token
+                        char *patientName = token;
 
-                      token = strtok(NULL, "|");
-                      int patientAge = atoi(token);
+                        token = strtok(NULL, "|");
+                        int patientAge = atoi(token);
 
-                      token = strtok(NULL, "|");
-                      char* patientDiagnosis = token;
+                        token = strtok(NULL, "|");
+                        char *patientDiagnosis = token;
 
-                      token = strtok(NULL, "|");
-                      int patientRoomNum = atoi(token);
+                        token = strtok(NULL, "|");
+                        int patientRoomNum = atoi(token);
 
-                      Patient newPatient;
+                        Patient newPatient;
 
-                      newPatient.patientId = patientID;
-                      strcpy(newPatient.name, patientName);
-                      newPatient.age = patientAge;
-                      newPatient.roomNumber = patientRoomNum;
+                        newPatient.patientId = patientID;
+                        strcpy(newPatient.name, patientName);
+                        newPatient.age = patientAge;
+                        newPatient.roomNumber = patientRoomNum;
+                        strcpy(newPatient.diagnosis, patientDiagnosis);
 
-                      add(thisPatient , newPatient);
+                        add(&thisPatient, newPatient);
                 }
         }
 }
 
 void updateFile(FILE *file, PatientNode patients)
 {
-        FILE* dataFile = fopen(FILE_NAME, "w"); // Open in write mode to overwrite
-        if(dataFile == NULL)
-        {
+        FILE *dataFile =
+                fopen(FILE_NAME, "w"); // Open in write mode to overwrite
+        if (dataFile == NULL) {
                 printf("Error opening file for overwriting\n");
                 return;
         }
         PatientNode *thisPatient = &patients; // Start from the first node
 
-        while (thisPatient != NULL)
-                {
-                        fprintf(dataFile, "%d|%s|%d|%s|%d\n",
-                                thisPatient->data.patientId,
-                                thisPatient->data.name,
-                                thisPatient->data.age,
-                                thisPatient->data.diagnosis,
-                                thisPatient->data.roomNumber
-                                );
-                        thisPatient = thisPatient->next; // Move to next node
-                }
+        while (thisPatient != NULL) {
+                fprintf(dataFile, "%d|%s|%d|%s|%d\n",
+                        thisPatient->data.patientId, thisPatient->data.name,
+                        thisPatient->data.age, thisPatient->data.diagnosis,
+                        thisPatient->data.roomNumber);
+                thisPatient = thisPatient->next; // Move to next node
+        }
         fclose(dataFile); // Close file
 }
